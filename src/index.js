72b9c22d7aeb1a5fd338b5c38b659ac439a7d63d
John Hartert <j.blainehartert@gmail.com>
2017-09-18 16:25:23 -0400

    Adds input component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import SearchBar from 'search_bar';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDU__Ld0_TK8aKmPqcWkHlX3W9oHvvKedQ';
 
 
 // Create a new component. This component should produce some HTML.
 const App = () => {
-    return <div>Hello World!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 //Take this component's generated HTML and put it on the page (in the DOM)
